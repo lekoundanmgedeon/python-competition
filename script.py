5ee6a2f446020554d79cf6253705130c1110aa40
lekoundanmgedeon-desk <lekounda.n.m.gedeon@aims-senegal.org>
2026-06-15 20:24:44 +0000

    push files

--- script.py
+++ script.py
@@ -1,2 +1,15 @@
 
-print(" My name is Gedeon LEKOUNDA and I will be the winner of this edition")
+def fib_memo(n, memo={}):
+    if n <= 0:
+        return 1
+    if n <= 1:  # error 1
+        return n
+    if n in memo:
+        return memo[n]
+    memo[n] = fib_memo(n - 1) + fib_memo(n - 2)  # error 2
+    return memo[n]
+
+for i in range(8):
+    print(fib_memo(i), end=" ")
+print()
+
---
+++ script2.py
@@ -0,0 +1,12 @@
+
+
+
+def oridinary_number (items): 
+
+    for i in items :
+        result =(i*i)
+        print(result
+              )
+        return result 
+
+print(oridinary_number([2,4,6,8]))
